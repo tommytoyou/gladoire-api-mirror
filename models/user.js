c8e7915b4750af017e7a6da547c737ba8e49a480
iamniama <plectrumonthespectrum@gmail.com>
2021-03-08 10:41:53 -0700

    comments for user model fields

--- models/user.js
+++ models/user.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    full_name: {type: String},
-    display_name: {type: String, required: true},
-    email: {type: String, required: true},
+    full_name: {type: String},  //user's full name, optional
+    display_name: {type: String, required: true},  //nickname
+    email: {type: String, required: true},  // email, just like on the tin
     password: {type: String, required: true, minLength: 8},
-    salt: {type: String},
-    custom_fields: [String],
-    location: {type: String},
-    user_level: {type: Number, required: true, default: 0},
-    is_active: {type: Number, required: true, default: 0},
-    is_hidden: {type: Number, required: true, default: 0},
-    bg_urls: [String],
-    connected_users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    date_joined: {type: Date, default: Date.now()}
+    salt: {type: String},   //this will be for encryption of journals if we implement it
+    custom_fields: [String],  //stores an array of string values that represent the user's custom journal fields
+    location: {type: String}, // optional, where is the user on the planet?
+    user_level: {type: Number, required: true, default: 0}, //access level, determines permissions and admin access
+    is_active: {type: Number, required: true, default: 0}, //user is currently in meditation session, this is how connections can start
+    is_hidden: {type: Number, required: true, default: 0}, //user does not want to show up as active
+    bg_urls: [String], //youtube URLs for background music
+    connected_users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}], //friends list
+    date_joined: {type: Date, default: Date.now()}  //generated automatically on registration, date of joining
 })
 
 const User = mongoose.model('User', userSchema);
