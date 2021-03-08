40564184c85a3ecfec05ed2e2f73cd3b41a1d1ed
iamniama <plectrumonthespectrum@gmail.com>
2021-03-07 17:49:35 -0700

    working on journal controller

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("I am app.js...fear me lulz")
+// Imports
+require('dotenv').config()
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors')
+const passport = require('passport')
+require('./config/passport')(passport)
+
+// App Set up
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // JSON parsing
+app.use(cors()); // allow all CORS requests
+app.use(passport.initialize())
---
+++ controllers/journal.js
@@ -0,0 +1,39 @@
+//TODO: finish journal DB functions
+
+require('dotenv').config
+const db = require('../models')
+
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = process.env
+
+const passport = require('passport')
+
+
+const create_full = async(req,res)=>{
+    let newEntry = {
+        user: req.user.id,
+        duration: req.body.duration,
+        mood_pre: req.body.mood_pre,
+        mood_post: req.body.mood_post,
+        energy_pre: req.body.energy_pre,
+        energy_post: req.body.energy_post,
+        tags: req.body.tags,
+        location: req.body.location,
+        moon_phase: req.body.moon_phase,
+        notes: req.body.notes,
+        user_fields: req.body.user_fields,
+        bg_music: req.body.bg_music
+
+    }
+    res.json(await db.Journal.create(newEntry))
+}
+
+const create_post = async(req,res) => {
+    let newEntry = {
+        user: req.user.id,
+        duration: req.body.duration,
+        bg_music: req.body.bg_music,
+        moon_phase: req.body.moon_phase
+    }
+    res.json(await db.Journal.create(newEntry))
+}
